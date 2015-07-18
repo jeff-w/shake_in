50a9991482097fda3e4ac5bff62a1c7a193b8b64
Kevin Lu <kevlu94@gmail.com>
2015-07-18 10:08:46 -0700

    hello

--- backend/Shaker.py
+++ backend/Shaker.py
@@ -8,7 +8,11 @@ class Shaker:
 		self.isRecruiter = isRecruiter
 		
 	def isToOld(self):
-		return (datetime.datetime.now() - self.startTime).total_seconds() > 120 #two mins
+		return (datetime.datetime.now() - self.startTime).total_seconds() > 10 #two mins
 		
 	def isRecruiter(self):
+		print "here"
 		return self.isRecruiter
+
+	def __str__(self):
+		return self.userId + str(self.isRecruiter)
