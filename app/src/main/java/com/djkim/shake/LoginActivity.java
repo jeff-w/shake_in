6ed2639f137f0e3d9ce702b10a508f4b7eca13d4
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2015-07-17 23:47:00 -0700

    Registration flow has been added

--- app/src/main/java/com/djkim/shake/LoginActivity.java
+++ app/src/main/java/com/djkim/shake/LoginActivity.java
@@ -11,6 +11,8 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login_page);
     }
 
+    // TODO: Make a function that checks the credential of the user
+
     public void signinButtonClicked(View view) {
 
     }
