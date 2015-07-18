cf0f6970c076833ded0a0a0a27deefee64de3954
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2015-07-17 21:11:27 -0700

    Login page is done

--- app/src/main/java/com/djkim/shake/LoginActivity.java
+++ app/src/main/java/com/djkim/shake/LoginActivity.java
@@ -2,11 +2,16 @@ package com.djkim.shake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+    }
+
+    public void signinButtonClicked(View view) {
+
     }
 }
---
+++ app/src/main/java/com/djkim/shake/SignupActivity.java
@@ -0,0 +1,10 @@
+package com.djkim.shake;
+
+import android.app.Activity;
+
+/**
+ * Created by dongjoonkim on 7/17/15.
+ */
+public class SignupActivity extends Activity {
+
+}
